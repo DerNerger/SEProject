5bfbcc0d90af4a5e38a1a45ad1fdc10b5f25adcb
Niklas Adams <adams.niklas@web.de>
2015-01-10 14:53:57 +0100

    Vision in der Gui

--- Evolution/src/de/evonomy/evolution/MapHolder.java
+++ Evolution/src/de/evonomy/evolution/MapHolder.java
@@ -26,7 +26,8 @@ public class MapHolder {
 	private static final String SPEZIESTWO = "#060606";
 	private static final String SPEZIESTHREE = "#F205DE";
 	private static final String SPEZIESFOUR = "#FF8400";
-	private static int ALPHA=255;
+	private static int ALPHA = 255;
+	private static int FOGOFWARALPHA = 150;
 	private FieldRect[][] mapFields;
 	private MapArea[] areas;
 	private ObjectAnimator anim;
@@ -36,9 +37,10 @@ public class MapHolder {
 	private final Paint black = new Paint();
 	private Paint color;
 	private Paint white;
-	SurfaceHolder mapHolder;
-	SurfaceHolder selHolder;
-	SurfaceHolder pointsHolder;
+	private SurfaceHolder mapHolder;
+	private SurfaceHolder selHolder;
+	private SurfaceHolder pointsHolder;
+	private SurfaceHolder fogHolder;
 	private boolean mapToBeDrawn = true;
 	private float heightPerBlock;
 	private float widthPerBlock;
@@ -54,14 +56,16 @@ public class MapHolder {
 	// Holds current evolutions of species
 	private ArrayList<PossibleUpdates> mySkills;
 
-	public MapHolder(SurfaceView mapHolder, SurfaceView selHolder,
+	public MapHolder(SurfaceView mapHolder,SurfaceView fogHolder, SurfaceView selHolder,
 			SurfaceView pointsHolder, int height, int width, int dispWidth,
 			int dispHeight, int[][] areasOfFields, LandType[] areasLandType,
 			Species species[]) {
 		this.mapHolder = mapHolder.getHolder();
+		this.fogHolder= fogHolder.getHolder();
 		this.selHolder = selHolder.getHolder();
 		this.pointsHolder = pointsHolder.getHolder();
 		this.mapHolder.setFormat(PixelFormat.TRANSPARENT);
+		this.fogHolder.setFormat(PixelFormat.TRANSPARENT);
 		this.selHolder.setFormat(PixelFormat.TRANSPARENT);
 		this.pointsHolder.setFormat(PixelFormat.TRANSPARENT);
 		initColors();
@@ -93,9 +97,9 @@ public class MapHolder {
 						.getRect());
 			}
 		}
-		for(int areac=0;areac<areas.length;areac++){
+		for (int areac = 0; areac < areas.length; areac++) {
 			Path path = calculatePath(areac);
-			Log.e("Path", path+" Area: "+areac);
+			Log.e("Path", path + " Area: " + areac);
 			areas[areac].setPath(path);
 		}
 		checkForAreasInAreasPath();
@@ -123,11 +127,11 @@ public class MapHolder {
 		FieldTypes.put(FieldType.ICE, ice);
 		Paint jungle = new Paint();
 		jungle.setColor(Color.parseColor("#1CA205"));
-		 jungle.setAlpha(ALPHA);
-		 desert.setAlpha(ALPHA);
-		 water.setAlpha(ALPHA);
-		 green.setAlpha(ALPHA);
-		 ice.setAlpha(ALPHA);
+		jungle.setAlpha(ALPHA);
+		desert.setAlpha(ALPHA);
+		water.setAlpha(ALPHA);
+		green.setAlpha(ALPHA);
+		ice.setAlpha(ALPHA);
 		FieldTypes.put(FieldType.JUNGLE, jungle);
 		speciesColors = new Paint[4];
 		speciesColors[0] = new Paint();
@@ -173,19 +177,26 @@ public class MapHolder {
 	 * */
 	public boolean drawMapLayout(boolean forced) {
 
-		if (mapToBeDrawn || forced ) {
+		if (mapToBeDrawn || forced) {
 			Canvas mapCanvas = mapHolder.lockCanvas();
 			if (mapCanvas == null)
 				return false;
 			mapCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
 			for (int x = 0; x < NuMBEROFBLOCKSWIDTH; x++) {
 				for (int y = 0; y < NUMBEROFBLOCKSHEIGHT; y++) {
-					if (mapFields[x][y].isVisible()) {
+					
 						Paint fieldColor = areas[mapFields[x][y].getArea()]
 								.getFieldType();
-						mapCanvas.drawRect(mapFields[x][y].getRect(),
-								fieldColor);
-					}
+//						if (!mapFields[x][y].isVisible()) {
+//							fieldColor.setAlpha(FOGOFWARALPHA);
+//							mapCanvas.drawRect(mapFields[x][y].getRect(),
+//									fieldColor);
+//							fieldColor.setAlpha(ALPHA);
+////						} else {
+							mapCanvas.drawRect(mapFields[x][y].getRect(),
+									fieldColor);
+//						}
+					
 				}
 			}
 
@@ -213,10 +224,10 @@ public class MapHolder {
 						if (tmp < 0)
 							break outerloop;
 						// draw a random 1x1
-						int xC = (int) (((Math.random() * (widthPerBlock - 1)))
-								+ x * widthPerBlock);
-						int yC = (int) (((Math.random() * (heightPerBlock - 1)))
-								+ y * heightPerBlock);
+						int xC = (int) (((Math.random() * (widthPerBlock - 1))) + x
+								* widthPerBlock);
+						int yC = (int) (((Math.random() * (heightPerBlock - 1))) + y
+								* heightPerBlock);
 						color = speciesColors[tmp];
 						color.setAlpha((int) /* percentage * */(alpha * 255));
 						pointsCanvas.drawRect(xC, yC, xC + (widthPerBlock / 4)
@@ -228,20 +239,40 @@ public class MapHolder {
 		}
 		pointsHolder.unlockCanvasAndPost(pointsCanvas);
 	}
+	public void drawFog(){
+		Canvas mapCanvas = fogHolder.lockCanvas();
+		if (mapCanvas == null)
+			return;
+		mapCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+		for (int x = 0; x < NuMBEROFBLOCKSWIDTH; x++) {
+			for (int y = 0; y < NUMBEROFBLOCKSHEIGHT; y++) {
+				int alpha;
+				if(mapFields[x][y].isVisible()) 
+					alpha=0; 
+				else 
+					alpha=FOGOFWARALPHA;
+				white.setAlpha(alpha);	
+//		
+						mapCanvas.drawRect(mapFields[x][y].getRect(),
+								white);
+//					}
+				
+			}
+		}
+		fogHolder.unlockCanvasAndPost(mapCanvas);
+	}
 
 	public void drawAreaLayout(int area) {
 		stopObjectAnimator();
 		areas[area].animateClicked(selHolder);
-		anim=ObjectAnimator.ofFloat(areas[area], "alpha", 0.30f,0.79f);
+		anim = ObjectAnimator.ofFloat(areas[area], "alpha", 0.30f, 0.79f);
 		anim.setInterpolator(new LinearInterpolator());
 		anim.setRepeatMode(ObjectAnimator.REVERSE);
 		anim.setDuration(1000);
 		anim.setRepeatCount(ObjectAnimator.INFINITE);
 		anim.start();
-		
-	}
 
-	
+	}
 
 	public FieldRect[][] getMapFields() {
 		return mapFields;
@@ -260,7 +291,7 @@ public class MapHolder {
 						1, 1, 1, 1, 1, 1, 1, 1, 0.1, 1, false);
 				Log.e("Species", "createdSpecies" + species[i]);
 			}
-			
+
 		}
 		this.species = species;
 		population = new long[4];
@@ -268,9 +299,9 @@ public class MapHolder {
 			population[i] = 0;
 		}
 		mySkills = new ArrayList<PossibleUpdates>();
-		data= new SpeciesData[4];
-		for(int i =0;i<data.length;i++){
-			data[i]= new SpeciesData(i);
+		data = new SpeciesData[4];
+		for (int i = 0; i < data.length; i++) {
+			data[i] = new SpeciesData(i);
 		}
 	}
 
@@ -299,7 +330,7 @@ public class MapHolder {
 	public void changePopulation(long[] population) {
 		for (int i = 0; i < population.length; i++) {
 			data[i].addPopulation(population[i]);
-			
+
 			this.population[i] = population[i];
 		}
 
@@ -338,195 +369,216 @@ public class MapHolder {
 	}
 
 	public int getArea(int x, int y) {
-		if(x >= NuMBEROFBLOCKSWIDTH || y >= NUMBEROFBLOCKSHEIGHT || x<0 || y<0) return -2;
+		if (x >= NuMBEROFBLOCKSWIDTH || y >= NUMBEROFBLOCKSHEIGHT || x < 0
+				|| y < 0)
+			return -2;
 		return mapFields[x][y].getArea();
 	}
-
-	
 
 	public boolean isReadyToDraw() {
 		return isReadyToDraw;
 	}
-	public void stopObjectAnimator(){
-		if(anim!=null){
+
+	public void stopObjectAnimator() {
+		if (anim != null) {
 			anim.cancel();
-			Canvas c=selHolder.lockCanvas();
+			Canvas c = selHolder.lockCanvas();
 			c.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
 			selHolder.unlockCanvasAndPost(c);
-			
+
 		}
 	}
-	public void destroyHolder(){
+
+	public void destroyHolder() {
 		stopObjectAnimator();
 	}
-	public Path calculatePath(int area){
-		Path p=new Path();
-		Point point=new Point(0,0);
-		Point startPoint=new Point(0,0);
-		//Start Punkt suchen
-		outerloop:
-		for(int i=0;i<NuMBEROFBLOCKSWIDTH;i++){
-			for(int j=0;j<NUMBEROFBLOCKSHEIGHT;j++){
-				if(mapFields[i][j].getArea()==area){
-					point.x=i;
-					point.y=j;
-					startPoint.x=i;
-					startPoint.y=j;
+
+	public Path calculatePath(int area) {
+		Path p = new Path();
+		Point point = new Point(0, 0);
+		Point startPoint = new Point(0, 0);
+		// Start Punkt suchen
+		outerloop: for (int i = 0; i < NuMBEROFBLOCKSWIDTH; i++) {
+			for (int j = 0; j < NUMBEROFBLOCKSHEIGHT; j++) {
+				if (mapFields[i][j].getArea() == area) {
+					point.x = i;
+					point.y = j;
+					startPoint.x = i;
+					startPoint.y = j;
 					break outerloop;
 				}
 			}
 		}
-		//path an den startpunkt bewegen
-		FloatPoint st= calculatePoint(point.x,point.y,true,true);
+		// path an den startpunkt bewegen
+		FloatPoint st = calculatePoint(point.x, point.y, true, true);
 		p.moveTo(st.x, st.y);
-		//Von da aus im Uhrzeigersinn nach nachbarpunkt suchen
-		int lastDir=2;
-		//0 für top, 1 für rechts, 2 für unten, 3 für links
-		int counter=0;
-		drawpath:
-		while(true){
+		// Von da aus im Uhrzeigersinn nach nachbarpunkt suchen
+		int lastDir = 2;
+		// 0 für top, 1 für rechts, 2 für unten, 3 für links
+		int counter = 0;
+		drawpath: while (true) {
 			counter++;
-			//letzte Richtung negieren
-			lastDir=(lastDir + 2) % 4;
-			//und eins weitergehen
-			
-			//prüfen, ob in dieser Richtung ein Feld der Area liegt, ansonsten eins weitergehen
-			//k is richtungscounter
-			//Log.e("Path","Aktueller Punkt: x "+ point.x+ " y "+ point.y +" dir: "+ lastDir);
+			// letzte Richtung negieren
+			lastDir = (lastDir + 2) % 4;
+			// und eins weitergehen
+
+			// prüfen, ob in dieser Richtung ein Feld der Area liegt, ansonsten
+			// eins weitergehen
+			// k is richtungscounter
+			// Log.e("Path","Aktueller Punkt: x "+ point.x+ " y "+ point.y
+			// +" dir: "+ lastDir);
 			int k;
-			Point newPoint=new Point(0,0);
-			for(k=0;k<4;k++){
-				lastDir=(lastDir+1) % 4;
-				if(lastDir==0){
-					if(checkArea(point.x,point.y-1,area)){
-						newPoint.x=point.x;
-						newPoint.y=point.y-1;
-						//direkt linie dahin zeichnen
-						FloatPoint g= calculatePoint(point.x,point.y,true,true);
+			Point newPoint = new Point(0, 0);
+			for (k = 0; k < 4; k++) {
+				lastDir = (lastDir + 1) % 4;
+				if (lastDir == 0) {
+					if (checkArea(point.x, point.y - 1, area)) {
+						newPoint.x = point.x;
+						newPoint.y = point.y - 1;
+						// direkt linie dahin zeichnen
+						FloatPoint g = calculatePoint(point.x, point.y, true,
+								true);
 						p.lineTo(g.x, g.y);
 						break;
-						
-					}else if(k>0){
-						//draw line to top Corner
-						FloatPoint g= calculatePoint(point.x,point.y,true,true);
+
+					} else if (k > 0) {
+						// draw line to top Corner
+						FloatPoint g = calculatePoint(point.x, point.y, true,
+								true);
+						p.lineTo(g.x, g.y);
+					}
+				} else if (lastDir == 1) {
+					if (checkArea(point.x + 1, point.y, area)) {
+						newPoint.x = point.x + 1;
+						newPoint.y = point.y;
+						FloatPoint g = calculatePoint(point.x, point.y, false,
+								true);
+						p.lineTo(g.x, g.y);
+						break;
+					} else if (k > 0) {
+						FloatPoint g = calculatePoint(point.x, point.y, false,
+								true);
+						p.lineTo(g.x, g.y);
+					}
+				} else if (lastDir == 2) {
+					if (checkArea(point.x, point.y + 1, area)) {
+						newPoint.x = point.x;
+						newPoint.y = point.y + 1;
+						// direkt linie dahin zeichnen
+						FloatPoint g = calculatePoint(point.x, point.y, false,
+								false);
+						p.lineTo(g.x, g.y);
+						break;
+
+					} else if (k > 0) {
+						FloatPoint g = calculatePoint(point.x, point.y, false,
+								false);
+						p.lineTo(g.x, g.y);
+					}
+				} else {
+					if (checkArea(point.x - 1, point.y, area)) {
+						newPoint.x = point.x - 1;
+						newPoint.y = point.y;
+						// direkt linie dahin zeichnen
+						FloatPoint g = calculatePoint(point.x, point.y, true,
+								false);
+						p.lineTo(g.x, g.y);
+						break;
+
+					} else if (k > 0) {
+						// draw line to top Corner
+						FloatPoint g = calculatePoint(point.x, point.y, true,
+								false);
 						p.lineTo(g.x, g.y);
 					}
 				}
-				else if(lastDir==1){
-					if(checkArea(point.x+1,point.y,area)){
-						newPoint.x=point.x+1;
-						newPoint.y=point.y;
-						FloatPoint g= calculatePoint(point.x,point.y,false,true);
-						p.lineTo(g.x, g.y);
-						break;
-					}
-					else if(k>0){
-						FloatPoint g= calculatePoint(point.x,point.y,false,true);
-						p.lineTo(g.x, g.y);
-					}
-				}else if(lastDir==2){
-					if(checkArea(point.x,point.y+1,area)){
-						newPoint.x=point.x;
-						newPoint.y=point.y+1;
-						//direkt linie dahin zeichnen
-						FloatPoint g= calculatePoint(point.x,point.y,false,false);
-						p.lineTo(g.x, g.y);
-						break;
-						
-					}else if(k>0){
-						FloatPoint g= calculatePoint(point.x,point.y,false,false);
-						p.lineTo(g.x, g.y);
-					}
-				}else{
-					if(checkArea(point.x-1,point.y,area)){
-						newPoint.x=point.x-1;
-						newPoint.y=point.y;
-						//direkt linie dahin zeichnen
-						FloatPoint g= calculatePoint(point.x,point.y,true,false);
-						p.lineTo(g.x, g.y);
-						break;
-						
-					}else if(k>0){
-						//draw line to top Corner
-						FloatPoint g= calculatePoint(point.x,point.y,true,false);
-						p.lineTo(g.x, g.y);
-					}
-				}
-				
-				
+
 			}
-			if(newPoint.x == startPoint.x && newPoint.y == startPoint.y){
+			if (newPoint.x == startPoint.x && newPoint.y == startPoint.y) {
 				break drawpath;
 			}
-			point.x=newPoint.x;
-			point.y=newPoint.y;
-			//if(counter>10000) break drawpath;
+			point.x = newPoint.x;
+			point.y = newPoint.y;
+			// if(counter>10000) break drawpath;
 		}
 		p.close();
-		
+
 		p.setFillType(Path.FillType.EVEN_ODD);
-		
+
 		return p;
 	}
-	private class Point{
+
+	private class Point {
 		int x;
 		int y;
-		public Point(int x, int y){
-			this.x=x;
-			this.y=y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
 	}
-	private class FloatPoint{
+
+	private class FloatPoint {
 		float x;
 		float y;
-		public FloatPoint(float x, float y){
-			this.x=x;
-			this.y=y;
+
+		public FloatPoint(float x, float y) {
+			this.x = x;
+			this.y = y;
 		}
 	}
-	private FloatPoint calculatePoint(int x, int y, boolean left, boolean top){
-		if(left && top){
-			return new FloatPoint(x*widthPerBlock,y*heightPerBlock);
-		}else if(!left && top){
-			return new FloatPoint((x+1)*widthPerBlock,y*heightPerBlock);
-		}else if(!left && !top){
-			return new FloatPoint((x+1)*widthPerBlock,(y+1)*heightPerBlock);
-		}else{
-			return new FloatPoint(x*widthPerBlock,(y+1)*heightPerBlock);
+
+	private FloatPoint calculatePoint(int x, int y, boolean left, boolean top) {
+		if (left && top) {
+			return new FloatPoint(x * widthPerBlock, y * heightPerBlock);
+		} else if (!left && top) {
+			return new FloatPoint((x + 1) * widthPerBlock, y * heightPerBlock);
+		} else if (!left && !top) {
+			return new FloatPoint((x + 1) * widthPerBlock, (y + 1)
+					* heightPerBlock);
+		} else {
+			return new FloatPoint(x * widthPerBlock, (y + 1) * heightPerBlock);
 		}
 	}
-	private boolean checkArea(int x, int y, int area){
-		if(x >= NuMBEROFBLOCKSWIDTH || y>= NUMBEROFBLOCKSHEIGHT
-				|| x < 0 || y < 0) return false;
-		if(mapFields[x][y].getArea() == area) return true;
+
+	private boolean checkArea(int x, int y, int area) {
+		if (x >= NuMBEROFBLOCKSWIDTH || y >= NUMBEROFBLOCKSHEIGHT || x < 0
+				|| y < 0)
+			return false;
+		if (mapFields[x][y].getArea() == area)
+			return true;
 		return false;
 	}
-	private void checkForAreasInAreasPath(){
-		arealoop:
-		for( int i=0;i<areas.length;i++){
-			for(int x=0;x<NuMBEROFBLOCKSWIDTH;x++){
-				for(int y=0;y<NUMBEROFBLOCKSHEIGHT;y++){
-					if(mapFields[x][y].getArea()==i){
-						//check all the neighbours
+
+	private void checkForAreasInAreasPath() {
+		arealoop: for (int i = 0; i < areas.length; i++) {
+			for (int x = 0; x < NuMBEROFBLOCKSWIDTH; x++) {
+				for (int y = 0; y < NUMBEROFBLOCKSHEIGHT; y++) {
+					if (mapFields[x][y].getArea() == i) {
+						// check all the neighbours
 						int nachbar;
-						for(int k=0;k<7;k++){
-							nachbar=calcNeigh(x,y,k);
-							//wen ungültig, ist am rand und damit nicht eingeshcloosen
-							if(nachbar == -2){
+						for (int k = 0; k < 7; k++) {
+							nachbar = calcNeigh(x, y, k);
+							// wen ungültig, ist am rand und damit nicht
+							// eingeshcloosen
+							if (nachbar == -2) {
 								areas[i].setEingeschlossenFalse();
 								continue arealoop;
 							}
-							//wenn in der gleichen Area
-							if(nachbar == i){ continue;
-							//wenn nicht prüfen, ob shcon ein nachbar gesetzt ist, wenn nicht, neu setzen,
-							//wenn shcon auf gleichheit prüfen und wenn nicht gleich 
-							}else{
-								if(areas[i].getNeighbour()==-1){
+							// wenn in der gleichen Area
+							if (nachbar == i) {
+								continue;
+								// wenn nicht prüfen, ob shcon ein nachbar
+								// gesetzt ist, wenn nicht, neu setzen,
+								// wenn shcon auf gleichheit prüfen und wenn
+								// nicht gleich
+							} else {
+								if (areas[i].getNeighbour() == -1) {
 									areas[i].setNeighbour(nachbar);
-								}else{
-									if(areas[i].getNeighbour()==nachbar){
+								} else {
+									if (areas[i].getNeighbour() == nachbar) {
 										continue;
-									}else{
+									} else {
 										areas[i].setEingeschlossenFalse();
 										continue arealoop;
 									}
@@ -537,31 +589,42 @@ public class MapHolder {
 				}
 			}
 		}
-		for(int area=0;area<areas.length;area++){
-			if(areas[area].isEingeschlossen()){
-				Path toEdit=areas[areas[area].getNeighbour()].getPath();
+		for (int area = 0; area < areas.length; area++) {
+			if (areas[area].isEingeschlossen()) {
+				Path toEdit = areas[areas[area].getNeighbour()].getPath();
 				toEdit.addPath(areas[area].getPath());
-				Path buffer=new Path();
+				Path buffer = new Path();
 				buffer.moveTo(0, 0);
 				buffer.close();
 				toEdit.addPath(buffer);
 			}
 		}
 	}
-	private int calcNeigh(int x, int y,int r){
-		switch(r){
-		case 0: return getArea(x+1,y);
-		case 1: return getArea(x+1,y+1);
-		case 2: return getArea(x+1,y-1);
-		case 3: return getArea(x,y+1);
-		case 4: return getArea(x,y-1);
-		case 5: return getArea(x-1,y);
-		case 6: return getArea(x-1,y+1);
-		case 7: return getArea(x-1,y-1);
-		default: return -2;
+
+	private int calcNeigh(int x, int y, int r) {
+		switch (r) {
+		case 0:
+			return getArea(x + 1, y);
+		case 1:
+			return getArea(x + 1, y + 1);
+		case 2:
+			return getArea(x + 1, y - 1);
+		case 3:
+			return getArea(x, y + 1);
+		case 4:
+			return getArea(x, y - 1);
+		case 5:
+			return getArea(x - 1, y);
+		case 6:
+			return getArea(x - 1, y + 1);
+		case 7:
+			return getArea(x - 1, y - 1);
+		default:
+			return -2;
 		}
 	}
-	public SpeciesData[] getSpeciesData(){
+
+	public SpeciesData[] getSpeciesData() {
 		return data;
 	}
 }
